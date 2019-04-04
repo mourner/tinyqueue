07002305870fa0f1612483c397645b97ceb33c30
Alexander Milevski <info@w8r.name>
2019-04-05 01:37:14 +0200

    Added TypeScript declaration file

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+export declare type Item = any;
+export declare type Comparator<Item> = (a: Item, b: Item) => -1 | 0 | 1;
+
+export default class TinyQueue<Item> {
+  public data : Item[];
+  public length : number;
+  constructor (items? : Item[], compare? : Comparator<Item>);
+  peek () : Item;
+  pop () : Item;
+  push (Item) : void;
+}
